7c9cf749c6fa6a9303f4ec92298b520860e2bb5b
dbregeon <denis.bregeon@digitalbrikes.com>
2020-08-30 15:01:16 +0900

    Multithreaded agent.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
+extern crate env_logger;
 extern crate libloading;
 extern crate log;
-extern crate env_logger;
 extern crate serde;
 extern crate serde_derive;
-extern crate toml;
 extern crate simple_error;
+extern crate toml;
 
 use libloading::{Library, Symbol};
-use spotr_sensing::{Sensor, Process};
-use log::{debug, info, warn};
+use log::{debug, error, info, trace, warn};
 use serde_derive::Deserialize;
+use spotr_sensing::{Sensor, SensorOutput};
+use std::any::Any;
+use std::sync::mpsc::{Receiver, Sender};
 use std::time::Duration;
 
 #[derive(Deserialize, Clone)]
 struct Config {
-    process_sensor: SensorConfig
+    process_sensor: SensorConfig,
 }
 
 impl std::fmt::Display for Config {
@@ -26,7 +28,7 @@ impl std::fmt::Display for Config {
 #[derive(Deserialize, Clone)]
 struct SensorConfig {
     sensor: String,
-    sample_interval: u64
+    sample_interval: u64,
 }
 
 impl SensorConfig {
@@ -37,68 +39,89 @@ impl SensorConfig {
 
 impl std::fmt::Display for SensorConfig {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "sensor: {}\nsample_interval: {:?}", self.sensor, self.sample_interval)
+        write!(
+            f,
+            "sensor: {}\nsample_interval: {:?}",
+            self.sensor, self.sample_interval
+        )
     }
 }
 
-struct AgentSensor<T> {
+struct AgentSensor {
+    name: String,
     lib: Library,
     interval: Duration,
-    sensor: Box<dyn Sensor<Item = T>>
+    sensor: Box<dyn Sensor>,
 }
 
-impl <T> AgentSensor<T> {
-    fn new(config: SensorConfig) -> Result<AgentSensor<T>, libloading::Error> {
-        let lib = Library::new(config.sensor.as_str())?;
+impl AgentSensor {
+    fn new(name: String, config: SensorConfig) -> AgentSensor {
+        let lib = Library::new(config.sensor.as_str())
+            .expect(format!("Missing library {}", config.sensor).as_str());
         let sensor = unsafe {
-            let initialize: Symbol<unsafe extern fn() ->  *mut dyn Sensor<Item = T>> = lib.get(b"initialize")?;
+            let initialize: Symbol<unsafe extern "C" fn() -> *mut dyn Sensor> = lib
+                .get(b"initialize")
+                .expect(format!("Failed to initialize {}", config.sensor).as_str());
             Box::from_raw(initialize())
         };
 
-        Ok(AgentSensor::<T> {
+        AgentSensor {
+            name: name,
             lib: lib,
             interval: config.interval(),
-            sensor: sensor
-        })
+            sensor: sensor,
+        }
     }
 
-    fn sample(&self) {
-        info!("Sampling");
-        self.sensor.sample().unwrap();
-        info!("Sampled");
-    }
-}
-
-struct Agent {
-    sensors: Vec<std::thread::JoinHandle<Result<(), simple_error::SimpleError>>>
-}
-
-impl Agent {
-    fn start(&mut self, config: Config) -> Result<(), simple_error::SimpleError> {
-        self.sensors.push(start::<Process>("process_sensor", config.process_sensor)?);
-        Ok(())
-    }
-}
-
-fn start<T>(name: &'static str, config: SensorConfig) -> Result<std::thread::JoinHandle<Result<(), simple_error::SimpleError>>, simple_error::SimpleError> {
-    match std::thread::Builder::new().name(name.to_string()).spawn(move || {
-        let sensor_result: Result<AgentSensor<T>, libloading::Error> = AgentSensor::<T>::new(config);
-        match sensor_result {
-            Ok(sensor) => {
-                loop {
-                    sensor.sample();
-                    std::thread::sleep(sensor.interval);
+    fn sample(&self, tx: &Sender<SensorOutput>) {
+        info!("{} sampling", self.name);
+        match self.sensor.sample() {
+            Ok(samples) => {
+                for sample in samples {
+                    match tx.send(sample) {
+                        Err(e) => error!("Failed to send sample: {}", e),
+                        _ => {}
+                    }
                 }
             }
-            Err(e) => Err(simple_error::SimpleError::new(format!("Failed to create the sensor {}: {}", name, e)))
+            Err(e) => error!("Sampling failed: {}", e),
         }
-        
-    }) {
-        Ok(handler) => Ok(handler),
-        Err(e) => Err(simple_error::SimpleError::new(format!("Failed to start the sensor thread for {}: {}", name, e)))
+        info!("{} sampled", self.name);
     }
 }
 
+fn start_publisher(
+    rx: Receiver<SensorOutput>,
+) -> std::thread::JoinHandle<Result<(), simple_error::SimpleError>> {
+    std::thread::Builder::new()
+        .name("publisher".to_string())
+        .spawn(move || {
+            for sample in rx.iter() {
+                match sample {
+                    SensorOutput::Process { pid } => println!("Process {}", pid),
+                };
+            }
+            Ok(())
+        })
+        .expect("Failed to start the publisher thread")
+}
+
+fn start_sensor(
+    name: &'static str,
+    config: SensorConfig,
+    tx: Sender<SensorOutput>,
+) -> std::thread::JoinHandle<Result<(), simple_error::SimpleError>> {
+    std::thread::Builder::new()
+        .name(name.to_string())
+        .spawn(move || {
+            let sensor = AgentSensor::new(name.to_string(), config);
+            loop {
+                sensor.sample(&tx);
+                std::thread::sleep(sensor.interval);
+            }
+        })
+        .expect(format!("Failed to start the sensor thread for {}", name).as_str())
+}
 
 fn main() {
     env_logger::init();
@@ -108,14 +131,15 @@ fn main() {
     info!("{}", config);
 
     info!("spotr-agent starting");
-    let mut agent = Agent {
-        sensors: vec!()
-    };
-    
-    agent.start(config).unwrap();
+    let (tx, rx): (Sender<SensorOutput>, Receiver<SensorOutput>) = std::sync::mpsc::channel();
+    let sensors = vec!(start_sensor("process_sensor", config.process_sensor, tx));
+    let publisher = start_publisher(rx);
 
-    for sensor in agent.sensors {
-        sensor.join().unwrap();
+    info!("spotr-agent started");
+
+    publisher.join().expect("Failed to join publisher thread");
+    for sensor in sensors {
+        sensor.join().expect("Failed to join sensor thread");
     }
 
     info!("spotr-agent exiting");
